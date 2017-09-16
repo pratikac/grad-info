eeddc3f9ab2aead39f8ffec7fa7da00d5f936f0c
pratik.ac <pratik.ac@gmail.com>
2017-09-15 20:31:31 -0700

    start long run

--- code/scripts/r1.sh
+++ code/scripts/r1.sh
@@ -1,7 +1,3 @@
-python compute_sigma.py -i '/local2/pratikac/results/allcnns/(Sep_08_02_44_57)_opt_{"b":128,"dataset":"cifar10","m":"allcnns","s":42}/allcnns_50.pz' -b 128
-
-python compute_sigma.py -i '/local2/pratikac/results/allcnns/(Sep_08_02_45_19)_opt_{"b":128,"dataset":"cifar100","m":"allcnns","s":42}/allcnns_50.pz' -b 128
-
-python compute_sigma.py -i '/local2/pratikac/results/allcnns/(Sep_08_02_44_57)_opt_{"b":128,"dataset":"cifar10","m":"allcnns","s":42}/allcnns_50.pz' -b 1024
-
-python compute_sigma.py -i '/local2/pratikac/results/allcnns/(Sep_08_02_45_19)_opt_{"b":128,"dataset":"cifar100","m":"allcnns","s":42}/allcnns_50.pz' -b 1024
+python compute_sigma.py -i '/local2/pratikac/results/allcnns/(Sep_08_02_44_57)_opt_{"b":128,"dataset":"cifar10","m":"allcnns","s":42}/allcnns_00.pz' -g 0 &
+python compute_sigma.py -i '/local2/pratikac/results/allcnns/(Sep_08_02_44_57)_opt_{"b":128,"dataset":"cifar10","m":"allcnns","s":42}/allcnns_30.pz' -g 1 &
+python compute_sigma.py -i '/local2/pratikac/results/allcnns/(Sep_08_02_44_57)_opt_{"b":128,"dataset":"cifar10","m":"allcnns","s":42}/allcnns_50.pz' -g 2 &
