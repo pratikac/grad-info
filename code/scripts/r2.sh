fee75f5d95ba715b0ce2b6bd33736e49e271134c
pratik.ac <pratik.ac@gmail.com>
2017-10-22 04:31:58 -0700

    minor, script launching

--- code/scripts/r2.sh
+++
@@ -1,3 +0,0 @@
-python compute_sigma.py -i '/home/pratik/Dropbox/iclr18data/allcnns/(Sep_08_02_44_57)_opt_{"b":128,"dataset":"cifar10","m":"allcnns","s":42}/S/allcnns_10.pz' --augment --stats
-python compute_sigma.py -i '/home/pratik/Dropbox/iclr18data/allcnns/(Sep_08_02_44_57)_opt_{"b":128,"dataset":"cifar10","m":"allcnns","s":42}/S/allcnns_20.pz' --augment --stats
-python compute_sigma.py -i '/home/pratik/Dropbox/iclr18data/allcnns/(Sep_08_02_44_57)_opt_{"b":128,"dataset":"cifar10","m":"allcnns","s":42}/S/allcnns_50.pz' --augment --stats
